77c846cb71b64c8f1008a683f4b4e928c6104233
Schnee Held <schneeheld@mail.ru>
2023-04-25 06:12:21 -0700

    Update printHeader.groovy

--- vars/printHeader.groovy
+++ vars/printHeader.groovy
@@ -1,5 +1,11 @@
 #!/usr/bin/env groovy
 
+import groovy.yaml.YamlBuilder
+
 def call(String caption = 'Title') {
   echo "---------------- ${caption} ----------------"
+  def yaml = new YamlBuilder()
+  yaml('---')
+  yaml('apiVersion: apps/v1')
+  println(yaml)
 }
